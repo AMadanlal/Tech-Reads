11a5df594c38115368b7e001af2e54e012a5f5c5
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-02-06 07:48:58 +0200

    Created more Views for the content of the application and further improved the front page UI

--- Tech Reads/ViewController.swift
+++ Tech Reads/ViewController.swift
@@ -14,39 +14,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        mainlabel.font=UIFont.italicSystemFont(ofSize: 50) //change the font of the main label
+        mainlabel.font=UIFont.italicSystemFont(ofSize: 100) //change the font of the main label
+        mainlabel.numberOfLines = 2
+        mainlabel.center.x = self.view.center.x
 
-        //    this is the source on how the following divider was created: https://www.dev.com/how-to-draw-rectangle-view-use-swift-in-ios-app/
-        
-//        the following is the starting co-ordinates of the rectangle/divider
-        let xcoordinate = 20
-        let ycoordinate = 285
-//        the sizes of the rectangle/divider
-        let rectanglewidth = 374
-        let rectangleheight = 1
-        
-//        now creating the object used to render the rectangle/divider
-        let divider : CGRect = CGRect(x : CGFloat(xcoordinate), y : CGFloat(ycoordinate), width : CGFloat(rectanglewidth), height: CGFloat(rectangleheight))
-        let dividerview=UIView(frame: divider)
-        dividerview.backgroundColor = UIColor.systemGray
-        self.view.addSubview(dividerview)
-        
+//    this is the source on how a divider is created: https://www.dev.com/how-to-draw-rectangle-view-use-swift-in-ios-app/
     }
 
-    @IBOutlet weak var ResultView: UIView!
-    
+//    this is a scroll view for the review of the game
+    @IBOutlet weak var SrollResultView: UIScrollView!
+
+    //    this is the app name label
     @IBOutlet weak var mainlabel: UILabel!
     
+//    this is the searchbar
     @IBOutlet weak var SearchBar: UISearchBar!
     
+//    this button is to change the user preferences
     @IBAction func PreferencesBtn(_ sender: Any){
     }
     
+//    this button is to change what is shown to the user
     @IBAction func GenTEchReviewBtn(_ sender: Any) {
     }
     
+//    this button is to change what is shown to the user
     @IBAction func GenGameReviewBtn(_ sender: Any) {
     }
     
+//    this is a view that shows the game trailer
+    @IBOutlet weak var GametrailerView: UIView!
+    
+//    this is to show the text for the actual game review in a dynamic way.
+    @IBOutlet weak var GameReviewText: UITextView!
 }
 
