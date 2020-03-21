b38e189a04055ccb44ff90b3918e733ddb55a8f7
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-03-21 16:51:53 +0200

    refactored code to work with objc

--- Tech ReadsTests/TextProcessingTests.swift
+++ Tech ReadsTests/TextProcessingTests.swift
@@ -17,7 +17,7 @@ class TextProcessingTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
            APIClass = ChickenCoopAPI(searched: "Mock Game", platform: "Any")
     }
-  
+
   func testFormatPlatformStringPC() {
    //    GIVEN
        let platform = "PC"
