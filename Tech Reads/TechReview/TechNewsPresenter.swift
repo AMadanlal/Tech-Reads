079114d98a3532bcdd93d8e34aa6bab4914ad351
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-04-17 10:40:32 +0200

    re-enabled the tech news button, deleted test file, added a tech news presenter file

---
+++ Tech Reads/TechReview/TechNewsPresenter.swift
@@ -0,0 +1,16 @@
+//
+//  Presenter.swift
+//  Tech Reads
+//
+//  Created by Akshar Madanlal on 2020/04/17.
+//  Copyright © 2020 Akshar Madanlal. All rights reserved.
+//
+
+import Foundation
+
+class TechNewsPresenter {
+
+  func displayTechNews(view : GeneralTechReview) {
+
+  }
+}
