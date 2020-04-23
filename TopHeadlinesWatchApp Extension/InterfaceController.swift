4dc16f50e0dcdff629f20833f1c513e167f7d8b9
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-04-23 16:40:24 +0200

    added apple watch files

---
+++ TopHeadlinesWatchApp Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TopHeadlinesWatchApp Extension
+//
+//  Created by Akshar Madanlal on 2020/04/23.
+//  Copyright © 2020 Akshar Madanlal. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import WatchConnectivity
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
